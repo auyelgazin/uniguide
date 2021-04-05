c193f471ca6268f9f87e1274d94514be20b26e1c
Kuanysh Auyelgazin <kuanyshauyelgazin@Kuanyshs-MacBook-Pro.local>
2021-04-05 16:12:47 +0600

    lang select completed

--- lib/screens/onboarding/choose_lang_screen.dart
+++ lib/screens/onboarding/choose_lang_screen.dart
@@ -4,6 +4,7 @@ import 'package:uniguide/controllers/language_controller.dart';
 import 'package:uniguide/widgets/font_styles.dart';
 import 'package:uniguide/widgets/wide_button_box.dart';
 import 'welcome_screen.dart';
+import 'package:uniguide/widgets/lang_button.dart';
 
 LanguageController _controller = Get.put(LanguageController());
 
@@ -28,50 +29,32 @@ class ChooseLangScreen extends StatelessWidget {
           SizedBox(
             height: 60,
           ),
-          WideButtonBox(
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFB7C1F4),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/en.png'),
-                    maxRadius: 15,
-                  ),
-                  Text(
-                    'English',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF141619),
-                    ),
-                  ),
-                  Icon(
-                    Icons.arrow_forward_ios,
-                    color: Color(0xFF232195).withOpacity(0.5),
-                  )
-                ],
-              ),
-            ),
+          LangButton(
+            langID: 'en',
+            region: 'US',
+            assetImage: 'images/en.png',
+            langName: 'English  ',
+            languageController: _controller,
           ),
           SizedBox(
             height: 21,
           ),
-          WideButtonBox(
-            Container(
-              child: LangButton('ru', 'RU', 'Русский'),
-            ),
+          LangButton(
+            langID: 'ru',
+            region: 'RU',
+            assetImage: 'images/ru.png',
+            langName: 'Русский',
+            languageController: _controller,
           ),
           SizedBox(
             height: 21,
           ),
-          WideButtonBox(
-            Container(
-              child: LangButton('kz', 'KZ', 'Қазақша'),
-            ),
+          LangButton(
+            langID: 'kz',
+            region: 'KZ',
+            assetImage: 'images/kz.png',
+            langName: 'Қазақша',
+            languageController: _controller,
           ),
         ],
       ),
@@ -79,24 +62,60 @@ class ChooseLangScreen extends StatelessWidget {
   }
 }
 
-class LangButton extends StatelessWidget {
-  final String lang;
-  final String region;
-  final String langName;
+// class LangButton extends StatelessWidget {
+//   final String langID;
+//   final String region;
+//   final String assetImage;
+//   final String langName;
 
-  LangButton(this.lang, this.region, this.langName);
+//   LangButton({this.langID, this.region, this.assetImage, this.langName});
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        primary: Color(0xFFB7C1F4),
-      ),
-      onPressed: () {
-        _controller.changeLanguage(lang, region);
-        Get.to(WelcomeScreen());
-      },
-      child: Text(langName),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         _controller.changeLanguage(langID, region);
+//         Get.to(() => WelcomeScreen());
+//       },
+//       child: WideButtonBox(
+//         Container(
+//           decoration: BoxDecoration(
+//             color: Color(0xFFB7C1F4),
+//             borderRadius: BorderRadius.circular(10),
+//           ),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: [
+//               SizedBox(
+//                 width: 5,
+//               ),
+//               Row(
+//                 children: [
+//                   CircleAvatar(
+//                     backgroundImage: AssetImage(assetImage),
+//                     maxRadius: 15,
+//                   ),
+//                   SizedBox(
+//                     width: 10,
+//                   ),
+//                   Text(
+//                     langName,
+//                     style: TextStyle(
+//                       fontSize: 22,
+//                       fontWeight: FontWeight.w400,
+//                       color: Color(0xFF141619),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               Icon(
+//                 Icons.arrow_forward_ios,
+//                 color: Color(0xFF232195).withOpacity(0.5),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/widgets/lang_button.dart
+++ lib/widgets/lang_button.dart
@@ -1,1 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:uniguide/controllers/language_controller.dart';
+import 'package:get/get.dart';
+import 'package:uniguide/screens/onboarding/welcome_screen.dart';
+import 'wide_button_box.dart';
+
+class LangButton extends StatelessWidget {
+  final String langID;
+  final String region;
+  final String assetImage;
+  final String langName;
+  final LanguageController languageController;
+
+  LangButton({this.langID, this.region, this.assetImage, this.langName, this.languageController});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        languageController.changeLanguage(langID, region);
+        Get.to(() => WelcomeScreen());
+      },
+      child: WideButtonBox(
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFB7C1F4),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: 5,
+              ),
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage(assetImage),
+                    maxRadius: 15,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    langName,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF141619),
+                    ),
+                  ),
+                ],
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: Color(0xFF232195).withOpacity(0.5),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
