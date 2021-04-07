72fb8d63b5723211555ceb009f534e76dd630b47
Kuanysh Auyelgazin <kuanyshauyelgazin@Kuanyshs-MacBook-Pro.local>
2021-04-07 15:06:19 +0600

    AppRoutes implementes

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uniguide/constants/lang/languages.dart';
-import 'package:uniguide/screens/auth/login_screen.dart';
-import 'package:uniguide/screens/auth/signup_screen.dart';
-import 'package:uniguide/screens/dashboard/dashboard_screen.dart';
-import 'package:uniguide/screens/onboarding/choose_lang_screen.dart';
+import 'package:uniguide/routes/app_routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:uniguide/screens/onboarding/welcome_screen.dart';
 
 int initScreen;
 
@@ -15,9 +11,8 @@ Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Firebase Core initialization
   FirebaseApp myFirebaseApp = await Firebase.initializeApp();
-  String name = myFirebaseApp.options.apiKey;
-  print(name);
 
   SharedPreferences preferences = await SharedPreferences.getInstance();
   initScreen = await preferences.getInt('initScreen');
@@ -26,20 +21,23 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+
+      // Localization based properties:
       locale: Locale('en', 'US'),
       fallbackLocale: Locale('en', 'US'),
       translations: Languages(),
+
       title: 'UniGuide',
       theme: ThemeData(
         fontFamily: 'SFPro',
         primarySwatch: Colors.blue,
       ),
-
-      home: initScreen == 0 || initScreen == null ? ChooseLangScreen() : LoginScreen(),
+      getPages: AppRoutes.list,
+      initialRoute: initScreen == 0 || initScreen == null ? '/chooseLang' : '/login',
+      // home: initScreen == 0 || initScreen == null ? ChooseLangScreen() : LoginScreen(),
       
     );
   }
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:uniguide/screens/auth/login_screen.dart';
+import 'package:uniguide/screens/auth/signup_screen.dart';
+import 'package:uniguide/screens/dashboard/dashboard_screen.dart';
+import 'package:uniguide/screens/onboarding/choose_lang_screen.dart';
+import 'package:uniguide/screens/onboarding/welcome_screen.dart';
+
+class AppRoutes {
+  static var list = [
+    GetPage(
+      name: '/chooseLang',
+      page: () => ChooseLangScreen(),
+    ),
+    GetPage(
+      name: '/welcome',
+      page: () => WelcomeScreen(),
+    ),
+    GetPage(
+      name: '/signup',
+      page: () => SignupScreen(),
+    ),
+    GetPage(
+      name: '/login',
+      page: () => LoginScreen(),
+    ),
+    GetPage(
+      name: '/',
+      page: () => DashboardScreen(),
+    ),
+  ];
+}
