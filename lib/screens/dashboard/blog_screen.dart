b403106ca72b806524691aa7173556789a2a958c
Kuanysh Auyelgazin <auyelgazin@gmail.com>
2021-04-15 22:23:51 +0600

    blog widget extracted

--- lib/screens/dashboard/blog_screen.dart
+++ lib/screens/dashboard/blog_screen.dart
@@ -71,140 +71,173 @@ class BlogScreen extends StatelessWidget {
               ],
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Container(
-              width: double.infinity,
-              child: Row(
+          Container(
+            height: 450,
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index){
+                return BlogCard();
+              },
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BlogCard extends StatelessWidget {
+  const BlogCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        width: double.infinity,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 15,
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    flex: 15,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CircleAvatar(),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    flex: 85,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xFFB7C1F4).withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 14, vertical: 11),
-                        child: Container(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 85,
-                                child: Container(
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        children: [
-                                          Row(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.center,
-                                            children: [
-                                              Text(
-                                                'Shawn Carter',
-                                                style: TextStyle(
-                                                    fontSize: 14,
-                                                    fontWeight:
-                                                        FontWeight.w600),
-                                              ),
-                                              SizedBox(width: 8),
-                                              Text(
-                                                '22:23',
-                                                style: TextStyle(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.w400,
-                                                  color: Color(0xFF687684),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                          SizedBox(height: 5),
-                                          Text(
-                                            'Also understand that, as humans, design can be emotional and stimulate the playful brain.',
-                                            style: TextStyle(
-                                                fontSize: 14,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(height: 10),
-                                      Row(children: [
-                                        Row(children: [
-                                          Icon(
-                                            Icons.comment_outlined,
-                                            color: Color(0xFF687684),
-                                          ),
-                                          SizedBox(
-                                            width: 6,
-                                          ),
-                                          Text(
-                                            '46',
-                                            style: TextStyle(
-                                                color: Color(0xFF687684),
-                                                fontSize: 12,
-                                                fontWeight: FontWeight.w400),
-                                          )
-                                        ]),
-                                        SizedBox(
-                                          width: 30,
-                                        ),
-                                        Row(children: [
-                                          Icon(
-                                            Icons.favorite_outline_rounded,
-                                            color: Color(0xFF687684),
-                                          ),
-                                          SizedBox(
-                                            width: 6,
-                                          ),
-                                          Text(
-                                            '363',
-                                            style: TextStyle(
-                                                color: Color(0xFF687684),
-                                                fontSize: 12,
-                                                fontWeight: FontWeight.w400),
-                                          )
-                                        ])
-                                      ])
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 15,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(Icons.more_horiz),
-                                      onPressed: () {},
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                  InkWell(
+                    child: CircleAvatar(),
+                    onTap: (){},
+                    splashColor: Colors.red,
                   ),
                 ],
               ),
             ),
-          )
-        ],
+            Expanded(
+              flex: 85,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFB7C1F4).withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 14, vertical: 11),
+                  child: Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 9,
+                          child: Container(
+                            child: Column(
+                              mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'Shawn Carter',
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight:
+                                                  FontWeight.w600),
+                                        ),
+                                        SizedBox(width: 8),
+                                        Text(
+                                          '22:23',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w400,
+                                            color: Color(0xFF687684),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      'Also understand that, as humans, design can be emotional and stimulate the playful brain.',
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 10),
+                                Row(children: [
+                                  InkWell(
+                                    child: Row(children: [
+                                      Icon(
+                                        Icons.comment_outlined,
+                                        color: Color(0xFF687684),
+                                      ),
+                                      SizedBox(
+                                        width: 6,
+                                      ),
+                                      Text(
+                                        '46',
+                                        style: TextStyle(
+                                            color: Color(0xFF687684),
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w400),
+                                      )
+                                    ]),
+                                  ),
+                                  SizedBox(
+                                    width: 30,
+                                  ),
+                                  InkWell(
+                                    child: Row(
+                                      children: [
+                                        Icon(
+                                          Icons.favorite_outline_rounded,
+                                          color: Color(0xFF687684),
+                                        ),
+                                        SizedBox(
+                                          width: 6,
+                                        ),
+                                        Text(
+                                          '363',
+                                          style: TextStyle(
+                                              color: Color(0xFF687684),
+                                              fontSize: 12,
+                                              fontWeight:
+                                                  FontWeight.w400),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ])
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: InkWell(
+                            onTap: () {},
+                            splashColor: Color(0xFFB7C1F4),
+                            child: Icon(
+                              Icons.more_horiz,
+                              color: Color(0xFF232195),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
