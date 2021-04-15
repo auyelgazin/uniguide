d3549e182154128fb3d0ee5a565440154ac43cf2
Kuanysh Auyelgazin <auyelgazin@gmail.com>
2021-04-15 21:45:50 +0600

    blog card ui ready

--- lib/screens/dashboard/blog_screen.dart
+++ lib/screens/dashboard/blog_screen.dart
@@ -8,6 +8,7 @@ class BlogScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.only(top: 20.0),
@@ -71,44 +72,138 @@ class BlogScreen extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-            child: Row(
-              children: [
-                Container(
-                  child: CircleAvatar(),
-                ),
-                Container(
-                  decoration: new BoxDecoration(
-                    color: Color(0xFFB7C1F4).withOpacity(0.3),
-                    borderRadius: new BorderRadius.circular(16),
+            padding: EdgeInsets.all(20),
+            child: Container(
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 15,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(),
+                      ],
+                    ),
                   ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Row(
-                        children: [
-                          Text('Name sur'),
-                          Text('23:34'),
-                          IconButton(icon: Icon(Icons.more), onPressed: () {})
-                        ],
+                  Expanded(
+                    flex: 85,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFB7C1F4).withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(16),
                       ),
-                      Container(
-                        width: 200,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 14, vertical: 11),
                         child: Container(
-                          padding: EdgeInsets.only(right: 20),
-                          child: Text(
-                            'disahdiashiud dsahudhau hsauhdusa hhdsuahduahudhushaud udh ausdhuas disahuidhasuidhudhuhdasd usdahduisa',
-                            textAlign: TextAlign.left,
-                            softWrap: true,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 85,
+                                child: Container(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        children: [
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                'Shawn Carter',
+                                                style: TextStyle(
+                                                    fontSize: 14,
+                                                    fontWeight:
+                                                        FontWeight.w600),
+                                              ),
+                                              SizedBox(width: 8),
+                                              Text(
+                                                '22:23',
+                                                style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Color(0xFF687684),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            'Also understand that, as humans, design can be emotional and stimulate the playful brain.',
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w400),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(height: 10),
+                                      Row(children: [
+                                        Row(children: [
+                                          Icon(
+                                            Icons.comment_outlined,
+                                            color: Color(0xFF687684),
+                                          ),
+                                          SizedBox(
+                                            width: 6,
+                                          ),
+                                          Text(
+                                            '46',
+                                            style: TextStyle(
+                                                color: Color(0xFF687684),
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w400),
+                                          )
+                                        ]),
+                                        SizedBox(
+                                          width: 30,
+                                        ),
+                                        Row(children: [
+                                          Icon(
+                                            Icons.favorite_outline_rounded,
+                                            color: Color(0xFF687684),
+                                          ),
+                                          SizedBox(
+                                            width: 6,
+                                          ),
+                                          Text(
+                                            '363',
+                                            style: TextStyle(
+                                                color: Color(0xFF687684),
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w400),
+                                          )
+                                        ])
+                                      ])
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 15,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(Icons.more_horiz),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
                           ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          ),
+          )
         ],
       ),
     );
