8155bbca562df7c2fce72bc2faf78e92f0f324f7
Kuanysh Auyelgazin <auyelgazin@gmail.com>
2021-05-01 12:14:54 +0600

    bkp /textfield hintlabel

--- lib/screens/dashboard/profile_screens/persontal_data_screen.dart
+++ lib/screens/dashboard/profile_screens/persontal_data_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uniguide/constants/font_styles.dart';
+import 'package:uniguide/widgets/auth_widgets/auth_textfield.dart';
 
 class PersonalDataScreen extends StatelessWidget {
+  final TextEditingController fullNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,41 +30,54 @@ class PersonalDataScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 59,
-                    backgroundImage: AssetImage('images/profile.png'),
-                  ),
-                  Positioned(
-                    right: 0,
-                    bottom: 0,
-                    child: Container(
-                      width: 36,
-                      height: 36,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.white, width: 1.5),
-                        color: Color(0xFF6200EE),
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        size: 22,
-                        color: Colors.white,
-                      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Stack(
+              children: [
+                CircleAvatar(
+                  radius: 59,
+                  backgroundImage: AssetImage('images/profile.png'),
+                ),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 36,
+                    height: 36,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 1.5),
+                      color: Color(0xFF6200EE),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 22,
+                      color: Colors.white,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Text('sd')
-          ],
-        ),
+          ),
+          Text('sd'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Text('yourname'),
+                AuthTextField(
+                  hintLabelText: null,
+                  hidePassword: false,
+                  controller: fullNameController,
+                  trailingIcon: null,
+                  keyboardType: TextInputType.name,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
